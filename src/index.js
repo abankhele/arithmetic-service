273a92a89752a85aa48b704972d8bfa9323b6d46
Soham Pingat <soham.pingat@gmail.com>
2024-02-15 16:43:42 -0500

    Hamya aaighala

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { add } = require("./arithmetica")
+const app = express();
+app.use(cors());
+const port = 3000;
+app.get('/',(req,res) => {
+    res.send('Arithmetic service - last updated TYPE-THE-DATE-AND-TIME-HERE');
+});
+
+app.get('/add/:n/:m', (req,res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sum = add(n, m);
+        res.json(sum);
+});
+
+app.listen(port, () => {
+     console.log('Server is running on port 3000');
+});
