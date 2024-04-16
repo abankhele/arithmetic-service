c5e677936fded1ab30885cd0e0402d8a8f6fdb58
Soham <soham.pingat@gmail.com>
2024-04-16 17:58:49 -0400

    Infra as code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const { add } = require("./arithmetica")
+const { add } = require("./arithmetica");
+
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(cors());
-const port = 3000;
-app.get('/',(req,res) => {
-    res.send('Arithmetic service - last updated TYPE-THE-DATE-AND-TIME-HERE');
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - last updated: ' + new Date().toISOString());
 });
 
-app.get('/add/:n/:m', (req,res) => {
+app.get('/add/:n/:m', (req, res) => {
     let n = Number(req.params.n);
     let m = Number(req.params.m);
     let sum = add(n, m);
-        res.json(sum);
+    res.json({ result: sum });
 });
 
 app.listen(port, () => {
-     console.log('Server is running on port 3000');
+    console.log('Server is running on port ' + port);
 });
